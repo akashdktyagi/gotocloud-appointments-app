a50c3f0b5684d87c0036b60682d8b3b2e3193351
akashtyagi <akashdktyagi@gmail.com>
2022-04-04 22:03:47 +0800

    multi module build - doctor api

---
+++ gotocloud-doctor-api/src/main/java/com/akashdktyagi/gotocloudspringbootapi1/GotocloudSpringbootApi1Application.java
@@ -0,0 +1,13 @@
+package com.akashdktyagi.gotocloudspringbootapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotocloudSpringbootApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotocloudSpringbootApi1Application.class, args);
+	}
+
+}
---
+++ gotocloud-doctor-api/src/test/java/com/akashdktyagi/gotocloudspringbootapi1/GotocloudSpringbootApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashdktyagi.gotocloudspringbootapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotocloudSpringbootApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
